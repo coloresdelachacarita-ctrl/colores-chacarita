1b52852516c2c7d0a8aab3510cad1f894169bb24
coloresdelachacarita-ctrl <coloresdelachacarita@gmail.com>
2025-09-08 17:22:19 -0300

    Update mapa.js

--- pages/pages/mapa.js
+++ pages/pages/mapa.js
@@ -1,45 +1,53 @@
-
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import L from "leaflet";
 
-const MapContainer = dynamic(() => import("react-leaflet").then(m => m.MapContainer), { ssr: false });
-const TileLayer    = dynamic(() => import("react-leaflet").then(m => m.TileLayer), { ssr: false });
-const Marker       = dynamic(() => import("react-leaflet").then(m => m.Marker), { ssr: false });
-const Popup        = dynamic(() => import("react-leaflet").then(m => m.Popup), { ssr: false });
+// Cargamos el mapa de manera dinámica para que funcione en Next.js
+const MapContainer = dynamic(
+  () => import("react-leaflet").then(mod => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then(mod => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then(mod => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(
+  () => import("react-leaflet").then(mod => mod.Popup),
+  { ssr: false }
+);
 
 export default function Mapa() {
   const [murales, setMurales] = useState([]);
 
   useEffect(() => {
     fetch("/murales.json")
-      .then(r => r.json())
-      .then(setMurales);
+      .then(res => res.json())
+      .then(data => setMurales(data));
   }, []);
-
-  const icon = new L.Icon({
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41]
-  });
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <MapContainer center={[-25.274, -57.635]} zoom={15} style={{ height: "100%", width: "100%" }}>
+      <MapContainer
+        center={[-25.2746, -57.6353]}
+        zoom={15}
+        style={{ height: "100%", width: "100%" }}
+      >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {murales.map(m => (
-          <Marker key={m.id} position={[m.lat, m.lng]} icon={icon}>
+        {murales.map((mural) => (
+          <Marker key={mural.id} position={[mural.lat, mural.lng]}>
             <Popup>
-              <strong>{m.nombre}</strong>
-              <p>{m.descripcion}</p>
+              <b>{mural.nombre}</b> <br />
+              {mural.descripcion}
             </Popup>
           </Marker>
         ))}
       </MapContainer>
     </div>
   );
-}
+    } Update mapa.js
