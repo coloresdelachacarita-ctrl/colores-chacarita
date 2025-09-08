6c9b0f8bed5d547316e7e043d7e9b28154ae1881
coloresdelachacarita-ctrl <coloresdelachacarita@gmail.com>
2025-09-08 16:11:02 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Circuito de Murales – Colores de la Chacarita</h1>
+      <p>Bienvenido a la primera versión de nuestra app en Next.js</p>
+    </div>
+  );
+}
